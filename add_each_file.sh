6ebcdf0c6842ab90e588b0f9b7325afa4ba48996
Aryan Godara <aryangodara03@gmail.com>
2023-10-20 00:57:57 +0530

    initial commit for add_each_file.sh

--- add_each_file.sh
+++ add_each_file.sh
@@ -1,6 +1,14 @@
-for file in *; do
-  if [ -f "$file" ]; then
-    git add "$file"
-    git commit -m "initial commit for $file"
-  fi
-done
+function git_add_commit {
+  for file in *; do
+    if [ -f "$file" ]; then
+      git add "$file"
+      git commit -m "initial commit for $file"
+    elif [ -d "$file" ]; then
+      cd "$file"
+      git_add_commit
+      cd ..
+    fi
+  done
+}
+
+git_add_commit
