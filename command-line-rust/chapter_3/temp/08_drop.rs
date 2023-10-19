e3f821e5bc30193f639b7cf25920aa3b58ac7291
Aryan Godara <aryangodara03@gmail.com>
2023-10-20 00:58:31 +0530

    initial commit for 08_drop.rs

---
+++ command-line-rust/chapter_3/temp/08_drop.rs
@@ -0,0 +1,40 @@
+struct Droppable {
+    name: &'static str,
+}
+
+// This trivial implementation of `drop` adds a print to console.
+impl Drop for Droppable {
+    fn drop(&mut self) {
+        println!("> Dropping {}", self.name);
+    }
+}
+
+fn main() {
+    let _a = Droppable { name: "a" };
+
+    // block A
+    {
+        let _b = Droppable { name: "b" };
+
+        // Block B
+        {
+            let _c = Droppable { name: "c" };
+            let _d = Droppable { name: "d" };
+
+            println!("Exiting block B");
+        }
+        println!("Just exited block B");
+
+        println!("Exiting block A");
+    }
+    println!("Just exited block A");
+
+    // Variable can be manually dropped using the `drop` function
+    drop(_a);
+    // TODO: ^ Try commenting this line
+    //* If we don't drop _a here, it gets dropped when exiting scope of main function.
+
+    println!("End of the main function");
+
+    // `_a` won't be `drop`ed again here, because it already han been (manually) `drop`ed
+}
